c41b4da2df8a93da46be78e67116919ea97e50f0
Ashish Gehani <ashish.gehani@sri.com>
2020-06-21 15:26:41 -0700

    Add tests for specialized versions of 'tar'

--- pytestbed/tar_tests/__init__.py
+++ pytestbed/tar_tests/__init__.py
@@ -4,11 +4,12 @@ import unittest
 
 # TODO: is there a way to automatically import scenario classes?
 
-from pytestbed.tar_tests.ScenarioTarStandard import standardScenario
+from pytestbed.tar_tests.ScenarioTarSpecialization import SpecializationScenario
 from pytestbed.tar_tests.ScenarioTarDisableCreateTarballs import disableCreatingTarballsScenario
 
 def load_tests(path):
-    suite = standardScenario(path)
+    suite = specializationScenario(path)
+    #suite = standardScenario(path)
     #suite.addTest(disableCreatingTarballsScenario(path))
     return suite
 
